5de0125cd5059bbf13ee2178058186be3d13ec6e
nanawanzii <nanawanzi9@gmail.com>
2025-04-14 18:28:31 +0800

    UPDATE

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import '../styles/Pages.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faBluesky, faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const PageFooter = () => {
   return (
     <footer className="contact-footer">
       <div className="contact-info-small">
         <p>
-          <i className="fas fa-envelope"></i> <a href="mailto:zi.wang.prince@hotmail.com">zi.wang.prince@hotmail.com</a>
+          <FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:zi.wang.prince@hotmail.com">zi.wang.prince@hotmail.com</a>
         </p>
         <p>
-          <a href="https://github.com/nanawanzii" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i> GitHub</a> | 
-          <a href="https://x.com/ZiWang23925033" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i> Twitter</a>
+          <a href="https://github.com/nanawanzii" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} /> GitHub
+          </a> | 
+          <a href="https://x.com/ZiWang23925033" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faXTwitter} /> X/Twitter
+          </a> | 
+          <a href="https://bsky.app/profile/ziwang.bsky.social" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faBluesky} /> BlueSky
+          </a>
         </p>
       </div>
     </footer>
